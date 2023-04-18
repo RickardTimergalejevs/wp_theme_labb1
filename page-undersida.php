44322f7ba3c164ed0eee4cb8a5eaece28f7823a9
RickardTimergalejevs <rickard.time@gmail.com>
2023-04-18 12:54:37 +0200

    undersida 1, content + navigation

---
+++ page-undersida.php
@@ -0,0 +1,31 @@
+<?php get_header() ?>
+
+<main>
+<section>
+	<div class="container">
+		<div class="row">
+			<div id="primary" class="col-xs-12 col-md-9">
+				<?php if (have_posts()) {
+					while (have_posts()) { ?>
+						<h1><?php the_title(); ?></h1>
+				<?php
+						the_post();
+						the_content();
+					}
+				} ?>
+			</div>
+
+			<aside id="secondary" class="col-xs-12 col-md-3">
+				<?php wp_nav_menu(
+					array(
+					'theme_location' => 'side-menu', 
+					'items_wrap' => '<ul class="side-menu">%3$s</ul>')
+					); ?>
+			</aside>
+
+		</div>
+	</div>
+</section>
+</main>
+
+<?php get_footer() ?>
