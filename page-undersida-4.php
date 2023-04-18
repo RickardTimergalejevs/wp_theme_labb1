62c3b80fe7a9b3af63cecb8be36063565abff8ef
RickardTimergalejevs <rickard.time@gmail.com>
2023-04-18 12:56:17 +0200

    undersida 4, content + image

---
+++ page-undersida-4.php
@@ -0,0 +1,28 @@
+<?php get_header(); ?>
+
+<main>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-md-6">
+          
+          <?php if (have_posts()) {
+            while (have_posts()) { ?>
+              <h1><?php the_title(); ?></h1>
+          <?php
+              the_post();
+              the_content();
+            }
+          } ?>
+        </div>
+
+        <div class="col-xs-12 col-sm-4 col-md-6 contact">
+          <img src="<?php the_post_thumbnail_url(); ?>">
+        </div>
+
+      </div>
+    </div>
+  </section>
+</main>
+
+<?php get_footer(); ?>
