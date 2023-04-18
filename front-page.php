a4b23e87ccc8bc453f6baba39ac59073b3759c29
RickardTimergalejevs <rickard.time@gmail.com>
2023-04-18 12:58:34 +0200

    front page content

---
+++ front-page.php
@@ -0,0 +1,29 @@
+<?php get_header() ?>
+
+<main>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <?php
+          if (have_posts()) {
+            while (have_posts()) {
+              the_post();
+          ?>
+              <div class="hero">
+                <img src="<?php the_post_thumbnail_url('full'); ?>">
+                <div class="text">
+                  <?php the_content(); ?>
+                </div>
+              </div>
+          <?php
+            }
+          }
+          ?>
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+
+<?php get_footer() ?>
